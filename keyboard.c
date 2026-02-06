betim05/wvkbd

commit b0eff372393a79e597c5c345c739b7d574f6c444

Send modifiers keyboard.key additionaly to keyboard.modifiers
At the moment we just use keyboard.modifiers to maintain our clients
modifiers states. But we should also send the associated key codes.
Clients like wlvncc need them to actually send the keys to the remote
compositor.

We got no clear way to map our modifier enums to key codes. So I added
this zwp_virtual_keyboard_v1_key_mods method.

I think I tried most of the use-cases. The hardest part was arround the
Shift+Space that send Tab. We have to depress the Shift key, reset the
modifiers, send the Tab key, and finally depress back Shift and its
modifier.

I've tested a bit the Code with .code_mod (and .reset_mod), but I'm not
that sure what their behavior should be.

Signed-off-by: Maarten van Gompel <proycon@anaproy.nl>

keyboard.c
@@ -275,11 +275,27 @@ kbd_get_layer_index(struct kbd *kb, struct layout *l)
 }
 
 void
+zwp_virtual_keyboard_v1_key_mods(struct zwp_virtual_keyboard_v1 *vkbd, uint32_t time, uint32_t mods, enum wl_keyboard_key_state state)
+{
+    if ((mods & Shift) == Shift)
+        zwp_virtual_keyboard_v1_key(vkbd, time, KEY_LEFTSHIFT, state);
+    if ((mods & CapsLock) == CapsLock)
+        zwp_virtual_keyboard_v1_key(vkbd, time, KEY_CAPSLOCK, state);
+    if ((mods & Ctrl) == Ctrl)
+        zwp_virtual_keyboard_v1_key(vkbd, time, KEY_LEFTCTRL, state);
+    if ((mods & Alt) == Alt)
+        zwp_virtual_keyboard_v1_key(vkbd, time, KEY_LEFTALT, state);
+    if ((mods & Super) == Super)
+        zwp_virtual_keyboard_v1_key(vkbd, time, KEY_LEFTMETA, state);
+    if ((mods & AltGr) == AltGr)
+        zwp_virtual_keyboard_v1_key(vkbd, time, KEY_RIGHTALT, state);
+}
+
+void
 kbd_unpress_key(struct kbd *kb, uint32_t time)
 {
-    bool unlatch_shift, unlatch_ctrl, unlatch_alt, unlatch_super, unlatch_altgr;
-    unlatch_shift = unlatch_ctrl = unlatch_alt = unlatch_super = unlatch_altgr = false;
-
+    bool unlatch_shift, unlatch_ctrl, unlatch_alt, unlatch_super, unlatch_altgr, unlatch_tab;
+    unlatch_shift = unlatch_ctrl = unlatch_alt = unlatch_super = unlatch_altgr = unlatch_tab = false;
     if (kb->last_press) {
         unlatch_shift = (kb->mods & Shift) == Shift;
         unlatch_ctrl = (kb->mods & Ctrl) == Ctrl;
@@ -287,22 +303,15 @@ kbd_unpress_key(struct kbd *kb, uint32_t time)
         unlatch_super = (kb->mods & Super) == Super;
         unlatch_altgr = (kb->mods & AltGr) == AltGr;
 
-        if (unlatch_shift) kb->mods ^= Shift;
-        if (unlatch_ctrl) kb->mods ^= Ctrl;
-        if (unlatch_alt) kb->mods ^= Alt;
-        if (unlatch_super) kb->mods ^= Super;
-        if (unlatch_altgr) kb->mods ^= AltGr;
-
-        if (unlatch_shift||unlatch_ctrl||unlatch_alt||unlatch_super||unlatch_altgr) {
-            zwp_virtual_keyboard_v1_modifiers(kb->vkbd, kb->mods, 0, 0, 0);
-        }
-
         if (kb->last_press->type == Copy) {
             zwp_virtual_keyboard_v1_key(kb->vkbd, time, 127, // COMP key
                                         WL_KEYBOARD_KEY_STATE_RELEASED);
         } else {
             if ((kb->shift_space_is_tab) && (kb->last_press->code == KEY_SPACE) && (unlatch_shift)) {
                 // shift + space is tab
+                unlatch_shift = false;
+                unlatch_tab = true;
+                kb->mods ^= Shift;
                 zwp_virtual_keyboard_v1_key(kb->vkbd, time, KEY_TAB,
                                             WL_KEYBOARD_KEY_STATE_RELEASED);
             } else {
@@ -312,10 +321,35 @@ kbd_unpress_key(struct kbd *kb, uint32_t time)
             }
         }
 
+        if (unlatch_shift) {
+            kb->mods ^= Shift;
+            zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, Shift, WL_KEYBOARD_KEY_STATE_RELEASED);
+        }
+        if (unlatch_ctrl) {
+            kb->mods ^= Ctrl;
+            zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, Ctrl, WL_KEYBOARD_KEY_STATE_RELEASED);
+        }
+        if (unlatch_alt) {
+            kb->mods ^= Alt;
+            zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, Alt, WL_KEYBOARD_KEY_STATE_RELEASED);
+        }
+        if (unlatch_super) {
+            kb->mods ^= Super;
+            zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, Super, WL_KEYBOARD_KEY_STATE_RELEASED);
+        }
+        if (unlatch_altgr) {
+            kb->mods ^= AltGr;
+            zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, AltGr, WL_KEYBOARD_KEY_STATE_RELEASED);
+        }
+
+        if (unlatch_shift||unlatch_ctrl||unlatch_alt||unlatch_super||unlatch_altgr||unlatch_tab) {
+            zwp_virtual_keyboard_v1_modifiers(kb->vkbd, kb->mods, 0, 0, 0);
+        }
+
         if (kb->compose >= 2) {
             kb->compose = 0;
             kbd_switch_layout(kb, kb->last_abc_layout, kb->last_abc_index);
-        } else if (unlatch_shift||unlatch_ctrl||unlatch_alt||unlatch_super||unlatch_altgr) {
+        } else if (unlatch_shift||unlatch_ctrl||unlatch_alt||unlatch_super||unlatch_altgr||unlatch_tab) {
             kbd_draw_layout(kb);
         } else {
             kbd_draw_key(kb, kb->last_press, Unpress);
@@ -392,6 +426,7 @@ kbd_press_key(struct kbd *kb, struct key *k, uint32_t time)
     switch (k->type) {
     case Code:
         if (k->code_mod) {
+            zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, k->code_mod, WL_KEYBOARD_KEY_STATE_PRESSED);
             if (k->reset_mod) {
                 zwp_virtual_keyboard_v1_modifiers(kb->vkbd, k->code_mod, 0, 0,
                                                   0);
@@ -406,7 +441,8 @@ kbd_press_key(struct kbd *kb, struct key *k, uint32_t time)
         kbd_draw_key(kb, k, Press);
         if ((kb->shift_space_is_tab) && (k->code == KEY_SPACE) && (kb->mods & Shift)) {
             // shift space is tab
-            zwp_virtual_keyboard_v1_modifiers(kb->vkbd, 0, 0, 0, 0);
+            zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, Shift, WL_KEYBOARD_KEY_STATE_RELEASED);
+            zwp_virtual_keyboard_v1_modifiers(kb->vkbd, kb->mods ^ Shift, 0, 0, 0);
             zwp_virtual_keyboard_v1_key(kb->vkbd, time, KEY_TAB,
                                         WL_KEYBOARD_KEY_STATE_PRESSED);
         } else {
@@ -423,6 +459,11 @@ kbd_press_key(struct kbd *kb, struct key *k, uint32_t time)
         break;
     case Mod:
         kb->mods ^= k->code;
+        if (kb->mods & k->code) {
+            zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, k->code, WL_KEYBOARD_KEY_STATE_PRESSED);
+        } else {
+            zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, k->code, WL_KEYBOARD_KEY_STATE_RELEASED);
+        }
         if ((k->code == Shift) || (k->code == CapsLock)) {
             kbd_draw_layout(kb);
         } else {

commit 49d8a629a5293692d67f89d362df3d5eaa2bd09d

fix: copy keys were broken with shift
A regression was introduced in b0eff372393a79e597c5c345c739b7d574f6c444
, leading to accented keys (accessed via Cmp) to only be available in
lower case, as shift had no effect there anymore. This should fix it.

Ref: https://github.com/jjsullivan5196/wvkbd/issues/115

keyboard.c
@@ -38,7 +38,7 @@ kbd_switch_layout(struct kbd *kb, struct layout *l, size_t layer_index)
     if ((!kb->prevlayout) ||
         (strcmp(kb->prevlayout->keymap_name, kb->layout->keymap_name) != 0)) {
         fprintf(stderr, "Switching to keymap %s\n", kb->layout->keymap_name);
-        create_and_upload_keymap(kb, kb->layout->keymap_name, 0, 0);
+        create_and_upload_keymap(kb, kb->layout->keymap_name, 0);
     }
     kbd_draw_layout(kb);
 }
@@ -198,7 +198,7 @@ kbd_init(struct kbd *kb, struct layout *layouts, char *layer_names_list,
     kb->last_abc_layout = &kb->layouts[layer];
 
     /* upload keymap */
-    create_and_upload_keymap(kb, kb->layout->keymap_name, 0, 0);
+    create_and_upload_keymap(kb, kb->layout->keymap_name, 0);
 }
 
 void
@@ -304,9 +304,11 @@ kbd_unpress_key(struct kbd *kb, uint32_t time)
         unlatch_altgr = (kb->mods & AltGr) == AltGr;
 
         if (kb->last_press->type == Copy) {
+            if (kb->debug) fprintf(stderr, "release copy key (unlatch_shift=%d, mods=%d)\n", unlatch_shift, kb->mods);
             zwp_virtual_keyboard_v1_key(kb->vkbd, time, 127, // COMP key
                                         WL_KEYBOARD_KEY_STATE_RELEASED);
         } else {
+            if (kb->debug) fprintf(stderr, "release key %d", kb->last_press->code);
             if ((kb->shift_space_is_tab) && (kb->last_press->code == KEY_SPACE) && (unlatch_shift)) {
                 // shift + space is tab
                 unlatch_shift = false;
@@ -322,6 +324,7 @@ kbd_unpress_key(struct kbd *kb, uint32_t time)
         }
 
         if (unlatch_shift) {
+            if (kb->debug) fprintf(stderr, "unlatch shift");
             kb->mods ^= Shift;
             zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, Shift, WL_KEYBOARD_KEY_STATE_RELEASED);
         }
@@ -460,8 +463,12 @@ kbd_press_key(struct kbd *kb, struct key *k, uint32_t time)
     case Mod:
         kb->mods ^= k->code;
         if (kb->mods & k->code) {
+            if (kb->debug)
+                fprintf(stderr, "mod pressed\n");
             zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, k->code, WL_KEYBOARD_KEY_STATE_PRESSED);
         } else {
+            if (kb->debug)
+                fprintf(stderr, "mod released\n");
             zwp_virtual_keyboard_v1_key_mods(kb->vkbd, time, k->code, WL_KEYBOARD_KEY_STATE_RELEASED);
         }
         if ((k->code == Shift) || (k->code == CapsLock)) {
@@ -523,10 +530,15 @@ kbd_press_key(struct kbd *kb, struct key *k, uint32_t time)
         // copy code as unicode chr by setting a temporary keymap
         kb->last_swipe = kb->last_press = k;
         kbd_draw_key(kb, k, Press);
-        if (kb->debug)
-            fprintf(stderr, "pressing copy key\n");
-        create_and_upload_keymap(kb, kb->layout->keymap_name, k->code,
-                                 k->code_mod);
+        if (kb->mods & Shift) {
+            if (kb->debug)
+                    fprintf(stderr, "Pressing copy key (with shift)\n");
+            create_and_upload_keymap(kb, kb->layout->keymap_name, k->code_mod
+        } else {
+            if (kb->debug)
+                    fprintf(stderr, "Pressing copy key\n");
+            create_and_upload_keymap(kb, kb->layout->keymap_name, k->code);
+        }
         zwp_virtual_keyboard_v1_modifiers(kb->vkbd, kb->mods, 0, 0, 0);
         zwp_virtual_keyboard_v1_key(kb->vkbd, time, 127, // COMP key
                                     WL_KEYBOARD_KEY_STATE_PRESSED);
@@ -699,8 +711,7 @@ draw_over_inset(struct drwsurf *ds, uint32_t x, uint32_t y, uint32_t width,
 }
 
 void
-create_and_upload_keymap(struct kbd *kb, const char *name, uint32_t comp_unichr,
-                         uint32_t comp_shift_unichr)
+create_and_upload_keymap(struct kbd *kb, const char *name, uint32_t comp_unichr)
 {
     int keymap_index = -1;
     for (int i = 0; i < NUMKEYMAPS; i++) {
@@ -715,7 +726,7 @@ create_and_upload_keymap(struct kbd *kb, const char *name, uint32_t comp_unichr,
     const char *keymap_template = keymaps[keymap_index];
     size_t keymap_size = strlen(keymap_template) + 64;
     char *keymap_str = malloc(keymap_size);
-    sprintf(keymap_str, keymap_template, comp_unichr, comp_shift_unichr);
+    sprintf(keymap_str, keymap_template, comp_unichr, comp_unichr);
     keymap_size = strlen(keymap_str);
     int keymap_fd = os_create_anonymous_file(keymap_size);
     if (keymap_fd < 0) {

keyboard.h
@@ -148,8 +148,7 @@ double kbd_get_row_length(struct key *k);
 void kbd_next_layer(struct kbd *kb, struct key *k, bool invert);
 void kbd_switch_layout(struct kbd *kb, struct layout *l, size_t layer_index);
 
-void create_and_upload_keymap(struct kbd *kb, const char *name,
-                              uint32_t comp_unichr, uint32_t comp_shift_unichr);
+void create_and_upload_keymap(struct kbd *kb, const char *name, uint32_t comp_unichr);
 
 #ifndef LAYOUT
 #error "make sure to define LAYOUT"